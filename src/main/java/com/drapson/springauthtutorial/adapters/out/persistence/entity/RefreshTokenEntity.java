225598964cf3c73fbf2dcb826cb80f3aa396c1b0
Krzysztof <krzysztof.drapson@gmail.com>
2025-06-19 00:11:50 +0200

    Create db entity representing user's auth provider

--- src/main/java/com/drapson/springauthtutorial/adapters/out/persistence/entity/RefreshTokenEntity.java
+++ src/main/java/com/drapson/springauthtutorial/adapters/out/persistence/entity/RefreshTokenEntity.java
@@ -20,7 +20,7 @@ public class RefreshTokenEntity {
     @Column(name = "expires_at", nullable = false)
     private LocalDateTime expiresAt;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private UserEntity user;
 
