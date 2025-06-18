a318398adb86f2054d756e4582a47dd4e4d31bbc
Krzysztof <krzysztof.drapson@gmail.com>
2025-06-19 01:34:01 +0200

    Add provider insert to db

--- src/main/java/com/drapson/springauthtutorial/adapters/out/persistence/repository/UserProviderRepositoryImpl.java
+++ src/main/java/com/drapson/springauthtutorial/adapters/out/persistence/repository/UserProviderRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.drapson.springauthtutorial.adapters.out.persistence.repository;
 
+import com.drapson.springauthtutorial.adapters.out.persistence.entity.UserOAuthProviderEntity;
+import com.drapson.springauthtutorial.adapters.out.persistence.mapper.UserOAuthProviderMapper;
 import com.drapson.springauthtutorial.application.UserProviderRepository;
+import com.drapson.springauthtutorial.application.dtos.UserOAuthProvider;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -17,5 +20,10 @@ public class UserProviderRepositoryImpl implements UserProviderRepository {
     @Override
     public boolean checkIfUserHasProvider(UUID id, String provider) {
         return jpaUserProviderRepository.existsByUserIdAndProvider(id, provider);
+    }
+
+    @Override
+    public void save(UserOAuthProvider userOAuthProvider) {
+        jpaUserProviderRepository.save(UserOAuthProviderMapper.toEntity(userOAuthProvider));
     }
 }
