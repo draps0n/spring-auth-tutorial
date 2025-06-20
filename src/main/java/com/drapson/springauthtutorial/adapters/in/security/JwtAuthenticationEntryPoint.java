27aecf1d407cfb082abd737e7c301d8a94b18498
Krzysztof <krzysztof.drapson@gmail.com>
2025-06-20 15:02:42 +0200

    Add proper handling for unauthorized requests

---
+++ src/main/java/com/drapson/springauthtutorial/adapters/in/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,45 @@
+package com.drapson.springauthtutorial.adapters.in.security;
+
+import com.drapson.springauthtutorial.adapters.in.api.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public JwtAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(
+                HttpStatus.UNAUTHORIZED,
+                errorCode.getDefaultMessage()
+        );
+        problem.setTitle(errorCode.getDefaultMessage());
+        problem.setType(URI.create("https://inz-api.com/errors/unauthorized"));
+        problem.setProperty("errorCode", errorCode.getCode());
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/problem+json");
+        response.setCharacterEncoding("UTF-8");
+
+        try (OutputStream os = response.getOutputStream()) {
+            objectMapper.writeValue(os, problem);
+            os.flush();
+        }
+    }
+}
